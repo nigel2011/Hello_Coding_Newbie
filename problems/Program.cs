a1b8ef481ee219647dc2ead504219606e2e33aaa
YeongDeuk <67935424+nigel2011@users.noreply.github.com>
2020-07-12 21:48:47 +0900

    Add files via upload

---
+++ problems/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 6-2 문제
+            Console.WriteLine("20을 초과하는 숫자를 입력하세요");
+            int userInput = int.Parse(Console.ReadLine());
+            if (userInput % 3 == 0)
+            {
+                Console.WriteLine("20보다 큰 3의 배수입니다.");
+                Console.WriteLine(userInput % 3);
+            }
+            else if (userInput % 3 != 0)
+            {
+                Console.WriteLine("20보다 크지만 3의 배수가 아닙니다.");
+            }
+            else if (userInput < 20)
+            {
+                Console.WriteLine("20보다 작은 숫자는 출력하지 않습니다.");
+            }
+        }
+    }
+}
+
+
